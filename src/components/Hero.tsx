1e495ce7389315006eedb73fed9771b74306c9af
satish <satishwillowood@gmail.com>
2024-02-07 22:31:42 +0530

    five days forecast done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import weathericon from '../assets/weather.png'
-import sunnyIcon from '../assets/sunny.png'
-import {newsApi, weatherApi} from '../constant/url.ts'
-
-
+import weathericon from "../assets/weather.png";
+import sunnyIcon from "../assets/sunny.png";
+import { newsApi, weatherApi } from "../constant/url.ts";
 
 interface NewsItem {
-    title: string;
-    urlToImage: string;
+  title: string;
+  urlToImage: string;
 }
-
 
 interface WeatherItem {
   title: string;
   weather: string;
   currentConditions: any | null;
+  days: any | null;
+  resolvedAddress: any | null;
 }
 
 const Hero: React.FC = () => {
@@ -26,9 +25,7 @@ const Hero: React.FC = () => {
   // calling the news api
   const getNewsData = async () => {
     try {
-      const res = await axios.get(
-        `${newsApi}`
-      );
+      const res = await axios.get(`${newsApi}`);
       const data = res.data.articles;
       const newData = data.filter(
         (item: NewsItem) => item.urlToImage !== null && item.urlToImage !== undefined
@@ -39,11 +36,18 @@ const Hero: React.FC = () => {
     }
   };
 
-  //calling the weather api 
+  //calling the weather api
   const getWeatherData = async () => {
     try {
+      const date = new Date();
+      const startDate = date.toISOString().slice(0, 10);
+      const endDate = new Date(date);
+      endDate.setDate(date.getDate() + 5);
+
+      const EndDate = endDate.toISOString().slice(0, 10);
+
       const res = await axios.get(
-        `${weatherApi}`
+        `${weatherApi}/India,Delhi/${startDate}/${EndDate}?key=4SZ6L9EPQV8GKM5LSSVGNJYYW&contentType=json`
       );
       const data = res.data;
       setWeather(data);
@@ -52,10 +56,9 @@ const Hero: React.FC = () => {
     }
   };
 
-
   useEffect(() => {
     getNewsData();
-    getWeatherData()
+    getWeatherData();
   }, []);
 
   const handleNext = () => {
@@ -68,50 +71,76 @@ const Hero: React.FC = () => {
     setActive(actIndex);
   };
 
-//   useEffect(() => {
-//     let timer = setTimeout(() => {
-//       handleNext();
-//     }, 4000);
-//     return () => clearTimeout(timer);
-//   }, [active]);
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      handleNext();
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [active]);
 
-console.log("Weather", weather)
+  console.log("Weather", weather);
 
-//Converting Epoch Time to Actual Day and Date
+  //Converting Epoch Time to Actual Day and Date
 
-const [formatData, setFormatDate] = useState("")
-useEffect(() => {
-  const sampleEpochTimestamp = weather?.currentConditions?.datetimeEpoch;
-  const date = new Date(sampleEpochTimestamp * 1000); 
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  };
-  const formattedDateString = date.toLocaleDateString(undefined, options);
-  setFormatDate(formattedDateString);
-}, [weather]);
+  const [formatData, setFormatDate] = useState("");
 
+  function convertEpochTime(epochTime: any) {
+    const sampleEpochTimestamp = epochTime;
+    const date = new Date(sampleEpochTimestamp * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric"
+    };
+    const formattedDateString = date.toLocaleDateString(undefined, options);
+    setFormatDate(formattedDateString);
+  }
+  useEffect(() => {
+    convertEpochTime(weather?.currentConditions?.datetimeEpoch);
+  }, [weather]);
+
+  // fahrenheit to celcius
+  function fahrenheit(fah: any) {
+    const celsius = Math.round(((fah - 32) * 5) / 9);
+    return celsius;
+  }
+
+  //epoch day convert
+
+  function epochDyConvert(epochTime: any) {
+    const sampleEpochTimestamp = epochTime;
+    const date = new Date(sampleEpochTimestamp * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long"
+    };
+    const formattedDateString = date.toLocaleDateString(undefined, options);
+    return formattedDateString;
+  }
 
   return (
     <>
       <section className="flex items-center justify-between w-full mt-16">
         <div className="flex items-start justify-between w-full gap-6">
-          <div className="hot news flex flex-col flex-1 lg:h-[500px] ">
+          <div className="hot news flex flex-col flex-1 lg:h-[465px] ">
             <h2 className="text-5xl font-robo font-bold">Hot Topics</h2>
             <div className=" relative images min-w-full min-h-full  rounded-md my-4 shadow-2xl">
               {images?.length
                 ? images.map((item, index) => (
                     <>
-                    <img
-                      key={index}
-                      style={{ display: active === index ? "block" : "none" }}
-                      src={item?.urlToImage}
-                      className=" w-full h-full object-fill aspect-[4/4] rounded-md shadow-md"
-                      alt={`News Image ${index + 1}`}
-                    />
-                    <h2 className="absolute blur-5 bottom-10 left-6 text-[36px] font-bold font-robo w-2/3 text-white" style={{ display: active === index ? "block" : "none" }} >{item?.title}</h2>
+                      <img
+                        key={index}
+                        style={{ display: active === index ? "block" : "none" }}
+                        src={item?.urlToImage}
+                        className=" w-full h-full object-fill aspect-[4/4] rounded-md shadow-md"
+                        alt={`News Image ${index + 1}`}
+                      />
+                      <h2
+                        className="absolute blur-5 bottom-10 left-6 text-[36px] font-bold font-robo w-2/3 text-white"
+                        style={{ display: active === index ? "block" : "none" }}
+                      >
+                        {item?.title}
+                      </h2>
                     </>
                   ))
                 : ""}
@@ -121,41 +150,34 @@ useEffect(() => {
             <div className="text-3xl text-start font-robo font-bold text-[#2F80ED]">{formatData}</div>
 
             <div className="flex items-center justify-between w-full rounded-md shadow-custom px-10 mt-12 py-5">
-                <div className="flex-col my-2">
-                    <h2 className="font-robo text-3xl font-bold">17 C</h2>
-                    <h2 className="font-robo text-lg font-bold">London, Uk</h2>
-                </div>
-                <div>
-                    <img src={weathericon}></img>
-                </div>
+              <div className="flex-col my-2 gap-2">
+                <h2 className="font-robo text-3xl font-bold">
+                  {fahrenheit(weather?.currentConditions?.temp)}&deg;
+                </h2>
+                <h2 className="font-robo text-lg font-bold">{weather?.resolvedAddress}</h2>
+              </div>
+              <div>
+                <img src={weathericon}></img>
+              </div>
             </div>
-            <div className="flex items-center justify-between w-full">
-                <div className="flex items-center justify-between w-1/2 px-2 my-3">
-                    <h2 className="text-xl font-robo font-bold text-[#7F7F7F]">Thursday</h2>
-                    <h3>
-                        <img src={sunnyIcon} alt="" />
-                    </h3>
+            {weather?.days?.slice(1,6).map((det: any) => (
+              <div className="flex items-center justify-between w-full mt-5">
+                <div className="flex items-center justify-between w-1/2 px-2 my-2">
+                  <h2 className="text-xl font-robo font-bold text-[#7F7F7F]">
+                    {epochDyConvert(det.datetimeEpoch)}
+                  </h2>
+                  <h3>
+                    <img src={sunnyIcon} alt="" />
+                  </h3>
                 </div>
                 <div className="flex items-center justify-between w-1/4 ">
-                    <h2 className="text-xl font-robo font-bold">13</h2>
-                    <h3 className="text-xl font-robo text-[#7F7F7F] font-bold">11</h3>
+                  <h2 className="text-xl font-robo font-bold">{fahrenheit(det?.tempmax)}&deg;</h2>
+                  <h3 className="text-xl font-robo text-[#7F7F7F] font-bold">
+                    {fahrenheit(det?.tempmin)}&deg;
+                  </h3>
                 </div>
-                
-            </div>
-
-            <div className="flex items-center justify-between w-full">
-                <div className="flex items-center justify-between w-1/2 px-2 my-3">
-                    <h2 className="text-xl font-robo font-bold text-[#7F7F7F]">Thursday</h2>
-                    <h3>
-                        <img src={sunnyIcon} alt="" />
-                    </h3>
-                </div>
-                <div className="flex items-center justify-between w-1/4 ">
-                    <h2 className="text-xl font-robo font-bold">13</h2>
-                    <h3 className="text-xl font-robo text-[#7F7F7F] font-bold">11</h3>
-                </div>
-                
-            </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
--- src/constant/url.ts
+++ src/constant/url.ts
@@ -1,3 +1,3 @@
-export const weatherApi = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/India,Hisar/2024-02-07/2024-02-11?key=4SZ6L9EPQV8GKM5LSSVGNJYYW&contentType=json`;
+export const weatherApi = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
 
 export const newsApi = `https://newsapi.org/v2/top-headlines?country=us&apiKey=6f4bc9b064a24c8093dd1bedbe9743d1`;
