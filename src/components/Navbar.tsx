e14021272f63fbce055e7f5afee8d66b5032a108
satish <satishwillowood@gmail.com>
2024-02-08 15:12:27 +0530

    search bar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,57 @@
+
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { MdOutlineLogout } from "react-icons/md";
+
 const Navbar = () => {
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchButtonClick = () => {
+    setIsSearchExpanded(!isSearchExpanded);
+  };
+
+  const handleSearchInputChange = (event: any) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    // Use the searchValue for further processing (e.g., search API)
+    console.log("Search Value:", searchValue);
+    // Reset the search input and collapse the search bar
+    setSearchValue("");
+    setIsSearchExpanded(false);
+  };
+
   return (
     <>
       <nav className="flex items-center justify-between w-full my-4 py-2 px-6 md:px-16">
         <div className="newsp flex items-center gap-2">
-          <button className="bg-[#121221] px-2 py-1 text-white rounded-md font-semibold font-rale">News</button>
+          <button className="bg-[#121221] px-2 py-1 text-white rounded-md font-semibold font-rale">
+            News
+          </button>
           <button className="bg-white px-2 py-1 text-black rounded-md font-semibold font-rale">Portal</button>
         </div>
         <div className="searchlogut flex items-center gap-4">
-          <button><IoIosSearch size={28}></IoIosSearch></button>
-          <button><MdOutlineLogout className="font-thin" size={28}></MdOutlineLogout></button>
+          <button className="searchbutton" onClick={handleSearchButtonClick}>
+            <IoIosSearch size={28}></IoIosSearch>
+          </button>
+
+          {isSearchExpanded && (
+            <div className="search-bar flex ">
+              <input
+                type="text"
+                placeholder="Search..."
+                value={searchValue}
+                onChange={handleSearchInputChange}
+                className="outline-none w-3/4 border-2 px-1.5 py-1.5 rounded-md"
+              />
+              <button className="bg-blue-400 px-2 py-1.5 rounded-md mx-2 text-white font-robo" onClick={handleSearchSubmit}>Search</button>
+            </div>
+          )}
+          <button>
+            <MdOutlineLogout className="font-thin" size={28}></MdOutlineLogout>
+          </button>
         </div>
       </nav>
     </>
