f0eb5be3bbbe16e4d49bd0d0d7fc1460fcf74391
satish <satishwillowood@gmail.com>
2024-02-14 18:03:18 +0530

    js file deleted

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Footer = () => {
-    return (React.createElement("footer", { className: "bg-gray-50  w-full " },
-        React.createElement("div", { className: "flex items-center justify-center py-6 " },
-            React.createElement("h2", { className: "font-rale font-bold text-gray-500" }, "Copyright 2021 News Portal"))));
-};
-export default Footer;
